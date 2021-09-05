b7d094962ad3e76c45f9809a8008ff04151002b3
Jonathan Moore <jtm170330@utdallas.edu>
2021-09-04 21:52:23 -0500

    Add text

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,35 @@ import React from 'react';
 import './App.css';
 import cube from './cube.svg'
 import styled from 'styled-components';
+// @ts-ignore
+import { Textfit } from 'react-textfit';
 
 function App() {
   return (
     <CubeBackground>
-
+      <StyledTextfit mode="single" forceSingleModeWidth={false} min={50} max={2000}>
+        9:52
+      </StyledTextfit>
     </CubeBackground>
   );
 }
 
+const StyledTextfit = styled(Textfit)`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  mix-blend-mode: hue;
+  color: #aaec84;
+  font-weight: 900;
+  font-family: 'Arial Black', 'Arial', sans-serif
+`;
+
 const CubeBackground = styled.div`
-  background-image: url(${cube});
+  background: url(${cube});
   background-repeat: repeat;
   width: 100vw;
   height: 100vh;
+  filter: hue-rotate(30deg);
 `;
 
 export default App;
