3656fcc03e15d2a02809c2e2d8f4b1a50a3cfead
Jonathan Moore <jtm170330@utdallas.edu>
2021-09-04 20:36:42 -0500

    Add cube

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import cube from './cube.svg'
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CubeBackground>
+
+    </CubeBackground>
   );
 }
+
+const CubeBackground = styled.div`
+  background-image: url(${cube});
+  background-repeat: repeat;
+  width: 100vw;
+  height: 100vh;
+`;
 
 export default App;
